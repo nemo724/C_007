0884328795cb80c3a24a680330374b682ed5412a
nemo <nemo724@naver.com>
2024-12-02 21:59:28 +0900

    x,y 좌표를 x,y축 범위에 따라 생성하는 함수 make_point 추가

--- 007번/a007_monteCarlopi.c
+++ 007번/a007_monteCarlopi.c
@@ -4,10 +4,14 @@
 #include <math.h>
 typedef struct { int x, y; }POINT;
 
+void make_point(POINT *);//x,y의 좌표를 결정하는 함수
 
 
 
-
+void make_point(POINT* pp)
+{pp->x = rand() % 100;//2*radius==원을 내접하고 있는 사각형 한 변의 길이(=이는 유효한 x축의 길이로 최소 0에서 최대 100까지 나올 수 있다)
+pp->y = rand() % 100;//2*radius==원을 내접하고 있는 사각형 한 변의 길이(=이는 유효한 y축의 길이로 최소 0에서 최대 100까지 나올 수 있다)
+}
 
 
 
@@ -31,6 +35,9 @@ int ocnt = 0;
 int radius = 50;
 double pi;
 
+srand((unsigned int)time(NULL));
+
+make_point(&p);
 
 
 
